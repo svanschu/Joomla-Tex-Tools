1363fd5fc977ce5958941d20fe27c4f83292cf82
Sven Schultschik <sven@schultschik.de>
2021-02-13 14:53:46 +0100

    load kunenatex ckeditor pluginfrom external

--- src/media/plg_kunenatex/js/katexck.js
+++ src/media/plg_kunenatex/js/katexck.js
@@ -7,11 +7,6 @@ function kaTeXReady(fn) {
 };
 
 function katexckeditorconf() {
-    console.log(CKEDITOR.instances);
-    CKEDITOR.on('loaded', function () {
-        //CKEDITOR.plugins.addExternal('katexck', 'plugins/kunena/kunenatex/katexck/');
-    });
-
     CKEDITOR.on('instanceReady', function () {
         editor = CKEDITOR.instances["message"];
 
@@ -20,11 +15,10 @@ function katexckeditorconf() {
         if (!config.extraPlugins.includes("katexck")) {
 
             config.extraPlugins += ",katexck";
-            console.log(config.extraPlugins.toString());
-
-            console.log(editor.config.extraPlugins.toString());
 
             editor.destroy();
+
+            CKEDITOR.plugins.addExternal('katexck', '../../../plg_kunenatex/katexck/');
             CKEDITOR.replace('message', config);
         }
     });

94876d94685d25294362cf7532c1de8e30ade0d4
Sven Schultschik <sven@schultschik.de>
2025-01-12 18:30:18 +0100

    cleanup
    Signed-off-by: Sven Schultschik <sven@schultschik.de>

--- src/plugins/jatex/src/Extension/JaTeX.php
+++ src/plugins/jatex/src/Extension/JaTeX.php
@@ -207,18 +207,6 @@ class JaTeX extends CMSPlugin implements SubscriberInterface
                         'integrity'   => 'sha384-7zkQWkzuo3B5mTepMUcHkMB5jZaolc2xDwL6VFqjFALcbeS9Ggm/Yr2r3Dy4lfFg'
                     ]
                 );
-            // TODO        <script>
-//   window.WebFontConfig = {
-//     custom: {
-//       families: ['KaTeX_AMS', 'KaTeX_Caligraphic:n4,n7', 'KaTeX_Fraktur:n4,n7',
-//         'KaTeX_Main:n4,n7,i4,i7', 'KaTeX_Math:i4,i7', 'KaTeX_Script',
-//         'KaTeX_SansSerif:n4,n7,i4', 'KaTeX_Size1', 'KaTeX_Size2', 'KaTeX_Size3',
-//         'KaTeX_Size4', 'KaTeX_Typewriter'],
-//     },
-//   };
-// </script>
-// <script defer src="https://cdn.jsdelivr.net/npm/webfontloader@1.6.28/webfontloader.js" integrity="sha256-4O4pS1SH31ZqrSO2A/2QJTVjTPqVe+jnYgOWUVr7EEc=" crossorigin="anonymous"></script>
-
         }
 
         $wa->useScript('plg_jatex.katexdo.js');
